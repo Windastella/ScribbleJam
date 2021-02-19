12c0ce3394380d5739d3c92b3cf0ceb75a495894
17380339 <densolo777@gmail.com>
2021-02-19 21:02:24 +0300

    arrows hiding while player is interacting

--- Scripts/Figures.gd
+++ Scripts/Figures.gd
@@ -2,6 +2,8 @@ extends Node2D
 class_name Figures
 
 export (Array, Texture) var dialogues;
+signal show_dialog
+signal hide_dialog
 
 func _ready():
 	for tex in dialogues:
@@ -9,6 +11,10 @@ func _ready():
 		texrect.texture = tex;
 		$dialog/CenterContainer/GridContainer.add_child(texrect);
 	$AnimationPlayer.play("Default");
+	var players = get_tree().get_nodes_in_group("Player")
+	var player = players[players.size() - 1]
+	self.connect("show_dialog", player, "_on_show_dialog")
+	self.connect("hide_dialog", player, "_on_hide_dialog")
 
 func show_interaction():
 	$interact_sprite.show();
@@ -22,7 +28,7 @@ func _on_Interact_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton:
 		if(event.button_index == BUTTON_LEFT && event.pressed):
 			$interact_sprite.hide();
-
+			emit_signal("show_dialog")
 			$dialog.show();
 
 
@@ -30,5 +36,5 @@ func _on_Dialog_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton:
 		if(event.button_index == BUTTON_LEFT && event.pressed):
 			$interact_sprite.show();
-
+			emit_signal("hide_dialog")
 			$dialog.hide();
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -17,6 +17,10 @@ var right_body = null
 var left_body = null
 var up_body = null
 
+var should_show_right = false
+var should_show_left = false
+var should_show_up = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	rng.randomize()
@@ -25,6 +29,22 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _on_show_dialog():
+	should_show_left = $Arrows/Left.visible
+	should_show_right = $Arrows/Right.visible
+	should_show_up = $Arrows/Up.visible
+	hide_arrows()
+	
+func _on_hide_dialog():
+	if(should_show_left): $Arrows/Left.show()
+	if(should_show_right): $Arrows/Right.show()
+	if(should_show_up): $Arrows/Up.show()
+
+func hide_arrows():
+	$Arrows/Up.hide()
+	$Arrows/Left.hide()
+	$Arrows/Right.hide()
 
 # move by arrow
 func _on_Right_input_event(_viewport, event, _shape_idx):
